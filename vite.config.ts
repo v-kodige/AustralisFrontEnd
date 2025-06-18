a340b7961d46b3f355485d3930277fe74c01ef42
Ian Parr <ianjudparr@gmail.com>
2025-06-18 21:09:00 +0100

    Update site root "/"

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: "/australis-site-seeker/", 
+  base: "/", 
   server: {
     host: "::",
     port: 8080,
